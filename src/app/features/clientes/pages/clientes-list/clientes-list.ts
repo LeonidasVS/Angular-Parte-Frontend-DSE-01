d72c13207667884d8e24c1954d1d13d0c5851123
LeonidasVS <villafrancosibrian@gmail.com>
2026-04-07 20:54:15 -0600

    Se finaliza el componente Dashboard y se comienza a trabajar el componente Cliente List

--- src/app/features/clientes/pages/clientes-list/clientes-list.ts
+++ src/app/features/clientes/pages/clientes-list/clientes-list.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-clientes-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './clientes-list.html',
   styleUrl: './clientes-list.css',
 })
-export class ClientesList {}
+export class ClientesList {
+  Clientes=[
+    {codigo: 'C001', nombre: 'Juan Perez', dui: '12345678-9', telefono: '5555-1234', estado: 'Activo'},
+    {codigo: 'C002', nombre: 'Maria Lopez', dui: '98765432-1', telefono: '5555-5678', estado: 'Inactivo'},
+    {codigo: 'C003', nombre: 'Carlos Gomez', dui: '45678912-3', telefono: '5555-9012', estado: 'Activo'},
+  ]
+}
